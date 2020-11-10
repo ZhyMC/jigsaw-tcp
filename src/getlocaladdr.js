94e0f7f8ab310fe6be9e19d33aeaecc7d013c94c
zhymc <854185073@qq.com>
2020-11-10 16:44:15 +0800

    fix getlocaladdr

--- src/getlocaladdr.js
+++ src/getlocaladdr.js
@@ -7,6 +7,10 @@ function getLocalAddress(mac){
 	for(let int of its){
 		for(let o of int){
 			if(o.family!="IPv4")continue;
+			if(o.internal)continue;
+			if(o.mac=="00:00:00:00:00:00")continue;
+			if(o.address=="127.0.0.1")continue;
+
 
 			if(mac){
 				if(o.mac==mac)
